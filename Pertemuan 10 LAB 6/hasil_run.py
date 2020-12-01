ef07c0682eca63106feac908783c753a9d87d1b6
fahmiekoputrosantoso <fahmieko73@gmail.com>
2020-12-01 15:11:08 +0700

    upload tugas

---
+++ Pertemuan 10 LAB 6/hasil_run.py
@@ -0,0 +1,25 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\HP\OneDrive\Documents\Tugas pertemuan 10 [LAB 6]\Pertemuan 10 LAB 6\main.py
+PROGRAM MENAMPILKAN DAFATR NILAI MAHASISWA
+
+(L)lihat, (T)ambah, (U)bah, (H)apus, (K)eluar : t
+=======Tambah Data=======
+Nama                :  Fahmi Eko Putro Santoso
+Nim                 :  312010046
+Masukan Nilai Tugas :  90
+Masukan Nilai UTS   :  87
+Masukan Nilai UAS   :  90
+
+(L)lihat, (T)ambah, (U)bah, (H)apus, (K)eluar : l
+=======Daftar Nilai Mahasiswa=======
+================================================================================================
+ |NO   |     NAMA      |    NIM    |     TUGAS    |     UTS     |       UAS    |    AKHIR     | 
+================================================================================================
+ |  1  |  Fahmi Eko Putro Santoso | 312010046 |          90  |          87 |          90  |        88.95 |
+============================================================================================
+
+(L)lihat, (T)ambah, (U)bah, (H)apus, (K)eluar : k
+Keluar
+>>> 
